5d2499cd6994107656fb4abb17be8d8d2e7c36d7
simana_sch <tktossi@live.com>
2021-08-08 02:21:39 +0900

    使わなくなった関数を削除、gRPCで音声合成エンジンのリストを返す処理を追加

--- src/SpeechPipeServer/Program.cs
+++ src/SpeechPipeServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using Speech;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Helloworld;
+using Ttscontroller;
 
 namespace SpeechGrpcServer
 {
@@ -22,7 +24,33 @@ namespace SpeechGrpcServer
         // Server side handler for the SayHelloAgain RPC
         public override Task<HelloReply> SayHelloAgain(HelloRequest request, ServerCallContext context)
         {
+            Program.OneShotPlayMode("葵", request.Name);
             return Task.FromResult(new HelloReply { Message = "Hello again " + request.Name });
+        }
+    }
+
+    class TTSControllerImpl : TTSService.TTSServiceBase
+    {
+        public override Task<SpeechEngineList> getSpeechEngineDetail(SpeechEngineRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(GetLibraryList());
+        }
+
+        private static SpeechEngineList GetLibraryList()
+        {
+            var results = new SpeechEngineList();
+            var engines = SpeechController.GetAllSpeechEngine();
+            foreach (SpeechEngineInfo engineInfo in engines)
+            {
+                results.DetailItem.Add( new SpeechEngineList.Types.SpeechEngineDetail
+                {
+                    EngineName = engineInfo.EngineName,
+                    LibraryName = engineInfo.LibraryName,
+                    EnginePath = engineInfo.EnginePath,
+                    Is64BitProcess = engineInfo.Is64BitProcess
+                });
+            }
+            return results;
         }
     }
     class Program
@@ -37,72 +65,23 @@ namespace SpeechGrpcServer
             Server server = new Server
             {
                 Services = {
-                    Greeter.BindService(new GreeterImpl())
+                    Greeter.BindService(new GreeterImpl()),
+                    TTSService.BindService(new TTSControllerImpl())
                 },
                 Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
             };
             server.Start();
 
-            Console.WriteLine("Greeter server listening on port " + Port);
-            Console.WriteLine("Press any key to stop the server...");
+            Console.WriteLine("localhost:" + Port + "で接続待機中");
+            Console.WriteLine("なにかキーを押すとサーバーを閉じます");
             Console.ReadKey();
 
             server.ShutdownAsync().Wait();
-            //pipeServer = new NamedPipeServerStream(PipeName, PipeDirection.InOut, 2 );
-            //Console.WriteLine("サーバーを立ち上げた");
-            //// Wait for a client to connect
-            //pipeServer.WaitForConnection();
-            //try
-            //{
-            //    Console.WriteLine("接続できた");
-            //    // Stream for the request.
-            //    StreamReader sr = new StreamReader(pipeServer);
-            //    // Stream for the response.
-            //    StreamWriter sw = new StreamWriter(pipeServer);
-            //    sw.AutoFlush = true;
-            //    string line;
-            //    // Read and display lines from the file until the end of
-            //    // the file is reached.
-            //    while ((line = sr.ReadLine()) != null)
-            //    {
-            //        Console.WriteLine(line);
-            //        OneShotPlayMode("葵", line);
-            //        sw.WriteLine("以下を再生した: " + line);
-            //    }
-
-            //    // Read モードの場合にやりたい処理
-            //    //using (StreamWriter sw = new StreamWriter(pipeServer))
-            //    //{
-            //    //    sw.AutoFlush = true;
-            //    //    //Console.Write("Enter text: ");
-            //    //    //sw.WriteLine(Console.ReadLine());
-            //    //    string bit = Environment.Is64BitProcess ? "64 bit" : "32 bit";
-            //    //    Console.WriteLine($"※ このアプリケーションは {bit}プロセスのため、{bit}のライブラリのみが列挙されます。");
-            //    //    Console.WriteLine("-----");
-            //    //    var names = GetLibraryName();
-            //    //    foreach (var s in names)
-            //    //    {
-            //    //        Console.WriteLine(s);
-            //    //        sw.WriteLine(s);
-            //    //    }
-            //    //}
-            //}
-            //catch (IOException e)
-            //{
-            //    Console.WriteLine("ERROR: {0}", e.Message);
-            //}
-            //pipeServer.Close();
         }
 
 
-        private static string[] GetLibraryName()
-        {
-            var engines = SpeechController.GetAllSpeechEngine();
-            var names = from c in engines
-                        select c.LibraryName;
-            return names.ToArray();
-        }
-        private static void OneShotPlayMode(string libraryName, string text)
+
+        public static void OneShotPlayMode(string libraryName, string text)
         {
 
             //var engines = SpeechController.GetAllSpeechEngine();
@@ -120,47 +99,6 @@ namespace SpeechGrpcServer
             };
             engine.Play(text);
 
-        }
-
-        // Defines the data protocol for reading and writing strings on our stream
-        public class StreamString
-        {
-            private Stream ioStream;
-            private UnicodeEncoding streamEncoding;
-
-            public StreamString(Stream ioStream)
-            {
-                this.ioStream = ioStream;
-                streamEncoding = new UnicodeEncoding();
-            }
-
-            public string ReadString()
-            {
-                int len = 0;
-
-                len = ioStream.ReadByte() * 256;
-                len += ioStream.ReadByte();
-                byte[] inBuffer = new byte[len];
-                ioStream.Read(inBuffer, 0, len);
-
-                return streamEncoding.GetString(inBuffer);
-            }
-
-            public int WriteString(string outString)
-            {
-                byte[] outBuffer = streamEncoding.GetBytes(outString);
-                int len = outBuffer.Length;
-                if (len > UInt16.MaxValue)
-                {
-                    len = (int)UInt16.MaxValue;
-                }
-                ioStream.WriteByte((byte)(len / 256));
-                ioStream.WriteByte((byte)(len & 255));
-                ioStream.Write(outBuffer, 0, len);
-                ioStream.Flush();
-
-                return outBuffer.Length + 2;
-            }
         }
     }
 }
